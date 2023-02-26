bb65b660ad3c4ea2c11c4fa357c3d5ac23408d88
Camelia03 <camelia_weber@yahoo.com>
2023-02-26 13:28:46 +0000

    Hide questions at the end of the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,23 +19,36 @@ let startButton = document.getElementById("start-btn");
 let questionContainer = document.getElementById("question-container");
 let question = document.getElementById("question");
 let answerButtons = document.getElementsByClassName("answer-btn");
+addEventListeners();
 
-startButton.addEventListener("click", function () {
+function addEventListeners() {
+    startButton.addEventListener("click", onStartClicked)
+
+    for (let i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].addEventListener("click", function () {
+            onAnswerClicked(i);
+        })
+    }
+}
+
+function onStartClicked() {
     startButton.classList.add("hidden")
     questionContainer.classList.remove("hidden")
 
     showQuestion(questionIndex);
-})
+}
 
-for (let i = 0; i < answerButtons.length; i++) {
-    answerButtons[i].addEventListener("click", function () {
-        if (i == questions[questionIndex].correctAnswer) {
-            numberOfCorrectAnswers++;
-        }
-        questionIndex++
-        showQuestion(questionIndex);
-        console.log(numberOfCorrectAnswers)
-    })
+function onAnswerClicked(index) {
+    if (index == questions[questionIndex].correctAnswer) {
+        numberOfCorrectAnswers++;
+    }
+    if (questionIndex == questions.length - 1) {
+        questionContainer.classList.add("hidden")
+        return;
+    }
+    questionIndex++
+    showQuestion(questionIndex);
+    console.log(numberOfCorrectAnswers)
 }
 
 function showQuestion(questionNumber) {
