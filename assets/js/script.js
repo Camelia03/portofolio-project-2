a4158a0baebfc0fefc86efd31ea46a95158eac39
Camelia03 <camelia_weber@yahoo.com>
2023-02-26 12:59:05 +0000

    Implement logic for displaying a question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,38 @@
-console.log('Hello World!')
+let questions = [{
+    title: "What's your name?",
+    answers: ["Maria", "Ioan", "Mihai", "Elena"],
+}, {
+    title: "What's your dog's name?",
+    answers: ["Zara", "Bobby", "Lessie", "Toby"],
+}, {
+    title: "What's your best friend's name?",
+    answers: ["Adina", "Diana", "Cristina", "Rares"],
+}]
+
+let questionIndex = 0;
+
+
+let startButton = document.getElementById("start-btn");
+let questionContainer = document.getElementById("question-container");
+let question = document.getElementById("question");
+let answerButtons = document.getElementsByClassName("answer-btn");
+
+startButton.addEventListener("click", function () {
+    startButton.classList.add("hidden")
+    questionContainer.classList.remove("hidden")
+
+    showQuestion(questionIndex);
+})
+
+for (let i = 0; i < answerButtons.length; i++) {
+    answerButtons[i].addEventListener("click", function () {
+        alert("Clicked button" + i)
+    })
+}
+
+function showQuestion(questionNumber) {
+    question.innerText = questions[questionNumber].title;
+    for (let i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].innerText = questions[questionNumber].answers[i];
+    }
+}
